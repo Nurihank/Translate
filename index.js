9f1972f72ed37039cfcd0469b02f348d5b5d2332
Nurihank <144276492+Nurihank@users.noreply.github.com>
2024-02-01 16:50:41 +0300

    doing the translation

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express")
+const translate = require('translate-google')
+const bodyParser = require("body-parser"); //postmanden gelenler body haline getirebilmek için
 
-const app = express();
+const app = express(); 
 
-app.use(express.json());
+app.use(express.json());    //verileri json formatına çeviriyor  
+app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
-app.listen(80,()=>{
-    console.log("80 port dinleniyor")
+app.listen(3000,()=>{ 
+    console.log("3000 port dinleniyor")
 })
 
+const msg = "book" //postmanden gelenleri alabilmek için
+const to = "ar"
+ 
+async function trans(){
+    const translation = await translate(msg, {from:"auto",to:to})
+    console.log(translation)
+}
+
+trans();
+
+
+app.post("/translate",async(req,res)=>{ 
+
+    const msg = req.body.msg //postmanden gelenleri alabilmek için
+    const to = req.body.to
+    
+    const translation = await translate(msg, {from:"auto",to:to})
+    res.status(200).json({translation})
+})
